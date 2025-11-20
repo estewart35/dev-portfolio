d86c91854e381ea02d83acbb7cddd9699f47a713
Ethan Stewart <ethan.stewart35@gmail.com>
2025-11-19 19:53:56 -0700

    Add vercel speed insights

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -49,6 +50,7 @@ export default function RootLayout({ children }) {
           <Footer />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
